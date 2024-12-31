27d674b1c9a3aa30f0c8a2a948029d101369172d
Parth Akre <127278085+parthakreumb@users.noreply.github.com>
2024-12-30 19:26:12 -0500

    Create 202. Happy Number.cpp

---
+++ 202. Happy Number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        set<int> usedNums;
+
+        while(true){
+
+            int sum = 0;
+            while(n!=0){
+              sum += ( n%10 ) * (n%10) ;
+              n = n/10;
+
+            }
+            if(sum ==1) return true;
+
+          
+
+            if(usedNums.find(sum) != usedNums.end()) return false;
+
+            usedNums.insert(sum);
+
+              n = sum;
+
+        }
+        
+    }
+};
